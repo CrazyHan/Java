f2d74b3699db7e58605533c253a57721adadfbd2
licong <licong1@lixiang.com>
2020-09-22 21:37:55 +0800

    家里面提交一下

---
+++ src/generic/type_inference_06/Box.java
@@ -0,0 +1,21 @@
+package generic.type_inference_06;
+
+public class Box<T> {
+    private T t;
+
+    public Box() {
+
+    }
+
+    public Box(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+
+    public void set(T t) {
+        this.t = t;
+    }
+}
