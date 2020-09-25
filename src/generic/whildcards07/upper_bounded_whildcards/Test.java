48cd42bb8e9778d48c0bde3a657cb96178c373ea
myName <hanshenglong001@gmail.com>
2020-09-25 16:35:54 +0800

    消费者，生产者

--- src/generic/whildcards07/upper_bounded_whildcards/Test.java
+++ src/generic/whildcards07/upper_bounded_whildcards/Test.java
@@ -18,6 +18,58 @@ public class Test {
         List<Integer> integers = Arrays.asList(1, 2, 3, 4);
         System.out.println("sum" + sumofList1(integers));
 
+
+        List<Son> sons = new ArrayList<>();
+        test(sons);
+
+        for (Son son : sons) {
+            System.out.println(son);
+        }
+        List<Daddy> daddies = new ArrayList<>();
+        test(daddies);
+
+        List<? extends Daddy> daddies1 = new ArrayList<>();
+        test(daddies1);
+
+
+        List<? extends Grandpa> grandpas = new ArrayList<>();
+        //test(grandpas);这个不行
+        List<Son> sons1 = new ArrayList<>();
+        test(sons1);
+
+        //这个玩意返回的不是 ArrayList 类型，不能添加
+//        List<Integer> src = Arrays.asList(new Integer(6),7,8,9,10);
+//        List<Integer> dst = Arrays.asList(new Integer(1),2,3,4,5);
+
+        List<Integer> src = new ArrayList<>();
+        src.add(6);
+        src.add(7);
+        src.add(8);
+        src.add(9);
+
+        List<Integer> dst = new ArrayList<>();
+        dst.add(1);
+        dst.add(2);
+        dst.add(3);
+        dst.add(4);
+        dst.add(5);
+
+
+        copy(src, dst);
+
+        System.out.println(dst);
+
+    }
+
+    //
+    public static <T> void copy(List<? extends T> productor, List<? super T> consumer) {
+//        System.arraycopy(productor, 0, consumer, consumer.size(), productor.size());
+
+        for (T t : productor) {
+            consumer.add(t);
+        }
+
+
     }
 
 
@@ -64,4 +116,23 @@ public class Test {
         return s;
     }
 
+    public static void test(List<? extends Daddy> daddies) {
+        daddies.add(null);
+        //这个出乎意料
+        List list = new ArrayList<>();
+        list.add(new Son());
+        daddies.addAll(list);//我草，这个可以添加这个呢绝对出问题
+    }
+
 }
+
+class Grandpa{
+
+}
+class Daddy extends Grandpa{
+
+}
+class Son extends Daddy{
+
+}
+
