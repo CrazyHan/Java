29488164ade466fca6cec493c007e46efaac31da
licong <licong1@lixiang.com>
2021-03-10 12:27:46 +0800

    使用CountDownLatch进行计数

--- src/thread/ThreadBase.java
+++ src/thread/ThreadBase.java
@@ -1,10 +1,63 @@
 package thread;
 
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 public class ThreadBase {
 
     public static void main(String[] args) throws InterruptedException {
 
+        testOrder2();
+
+    }
+
+    public static void testOrder2() {
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        Thread thread1 = new Thread("线程1") {
+            @Override
+            public void run() {
+                super.run();
+                System.out.println(getName() + "执行任务3秒");
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(getName() + "执行完成");
+                countDownLatch.countDown();
+            }
+        };
+        thread1.start();
+
+        Thread thread2 = new Thread("线程2") {
+            @Override
+            public void run() {
+                super.run();
+                System.out.println(getName() + "执行任务3秒");
+                try {
+                    sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(getName() + "执行完成");
+                countDownLatch.countDown();
+            }
+        };
+        thread2.start();
+
+        try {
+            countDownLatch.await(5, TimeUnit.SECONDS);
+            System.out.println(Thread.currentThread().getName() + "执行任务等不及了，后面先执行了"+countDownLatch.getCount());
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void testOrder(){
         System.out.println(Thread.currentThread().getName()+"排队打饭中");
 
         Thread thread1 = new Thread(){
@@ -60,14 +113,15 @@ public class ThreadBase {
 
         System.out.println(Thread.currentThread().getName() + "发现第二名来打饭，让他插队");
 
-        thread2.join();
+        try {
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
 
         System.out.println("自己打打饭");
-
     }
-
-
 
 
 }
